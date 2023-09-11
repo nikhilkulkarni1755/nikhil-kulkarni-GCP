63189b2a5b0f6d5632abcc4787336f38fa594340
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2023-09-11 17:17:44 -0400

    [Node] IpInfo

--- app-engine-node/index.js
+++ app-engine-node/index.js
@@ -1,3 +1,7 @@
+require("dotenv").config()
+
+//console.log(process.env.IP_INFO_KEY)
+
 let express = require('express')
 
 let app = express()
@@ -14,6 +18,14 @@ app.get('/angular', (req, res) => {
     res.send('<h1>This project has been created with Node and Angular</h1>')
 })
 
+app.post('*', function(req, res, next) {
+  if(getIP === null) {
+    console.log('did not find ip address')
+  }
+  var data = req.body.params
+  console.log(req.data)
+})
+
 // app.use('/angular', require(express.static('frontend')))
 
 app.use(express.static('frontend'))
@@ -21,3 +33,23 @@ app.use(express.static('frontend'))
 app.listen(port, () => {
     console.log('Node server is running @ http://localhost:4000')
 })
+
+function getIP() {
+    var token = process.env.IP_INFO_KEY
+    var promise = "https://ipinfo.io/json?token=" + token
+    console.log(promise)
+    fetch(promise).then(
+      (response) => response.json()
+    ).then(
+      (jsonResponse) => {
+        // console.log(jsonResponse.ip, jsonResponse.country)
+        location = jsonResponse.city + ", " + jsonResponse.region + ", " + jsonResponse.country
+        console.log(location)
+
+        return location
+      }
+    )
+    
+    return null
+
+}
