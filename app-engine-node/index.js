15c7e65f725bc2fb374865660f6378eb845bdcc9
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2023-09-13 20:58:49 -0400

    [Node][Angular] cors, ipinfo. Angular should add ipinfo too

--- app-engine-node/index.js
+++ app-engine-node/index.js
@@ -1,30 +1,53 @@
 require("dotenv").config()
 
+let cors = require('cors')
+
 //console.log(process.env.IP_INFO_KEY)
 
 let express = require('express')
 
 let app = express()
 
+app.use(cors())
+
 let normalizePort = require('normalize-port')
 
 var port = normalizePort(process.env.PORT || '4000');
 
-app.get('/test', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+// app.get('/test', (req, res) => {
+//     res.send('<h1>Hello World</h1>')
+// })
+
+app.get('/ip', (req, res) => {
+    // res.set('Access-Control-Allow-Origin', 'http://localhost:'+4200);
+
+    var token = process.env.IP_INFO_KEY
+    var promise = "https://ipinfo.io/json?token=" + token
+    // console.log(promise)
+
+    fetch(promise).then(
+      (response) => response.json()
+    ).then(
+      (jsonResponse) => {
+        // console.log(jsonResponse.ip, jsonResponse.country)
+        foundlocation = jsonResponse.city + ", " + jsonResponse.region + ", " + jsonResponse.country
+        // console.log('Location in fxn: ' + foundlocation)
+        res.send(foundlocation)
+      }
+    )
 })
 
 app.get('/angular', (req, res) => {
     res.send('<h1>This project has been created with Node and Angular</h1>')
 })
 
-app.post('*', function(req, res, next) {
-  if(getIP === null) {
-    console.log('did not find ip address')
-  }
-  var data = req.body.params
-  console.log(req.data)
-})
+// app.post('*', function(req, res, next) {
+//   if(getIP === null) {
+//     console.log('did not find ip address')
+//   }
+//   var data = req.body.params
+//   console.log(req.data)
+// })
 
 // app.use('/angular', require(express.static('frontend')))
 
@@ -34,22 +57,4 @@ app.listen(port, () => {
     console.log('Node server is running @ http://localhost:4000')
 })
 
-function getIP() {
-    var token = process.env.IP_INFO_KEY
-    var promise = "https://ipinfo.io/json?token=" + token
-    console.log(promise)
-    fetch(promise).then(
-      (response) => response.json()
-    ).then(
-      (jsonResponse) => {
-        // console.log(jsonResponse.ip, jsonResponse.country)
-        location = jsonResponse.city + ", " + jsonResponse.region + ", " + jsonResponse.country
-        console.log(location)
 
-        return location
-      }
-    )
-    
-    return null
-
-}
