bc635452a8bd03f1d11d2bb6a9b4a663b6870764
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2023-11-07 22:03:32 -0800

    [weather] added weather data. also paving the way for a contact form on the home page

--- app-engine-node/index.js
+++ app-engine-node/index.js
@@ -1,11 +1,13 @@
+
+// const https = require("https")
+
+const nodemailer = require('nodemailer')
 // import requestIp from 
 let requestIp = require('request-ip')
 
 require("dotenv").config()
 
 let cors = require('cors')
-
-//console.log(process.env.IP_INFO_KEY)
 
 let express = require('express')
 
@@ -20,30 +22,110 @@ let normalizePort = require('normalize-port')
 
 var port = normalizePort(process.env.PORT || '4000');
 
-// app.get('/test', (req, res) => {
-//     res.send('<h1>Hello World</h1>')
+// const request = await fetch("https://ipinfo.io/json?token="+ipKey)
+// const json = await request.json()
+
+app.get('/contactMe', (req, res) => {
+  
+})
+
+// app.get('/sendConfirm', (req, res) => {
+//   let randomValue = Math.floor(Math.random() * (999999 - 100001 + 1) + 100001)
+//   let time = Math.floor(Date.now()/1000)
+//   const transporter = nodemailer.createTransport({
+//     service:"gmail",
+//     auth: {
+//       user:"nodejsserverstuff@gmail.com",
+//       pass:process.env.SERVER_KEY
+//     }
+//   })
+
+//   const mailOptions = {
+//     from:"nodejsserverstuff@gmail.com",
+//     to:"nikhilkulkarni1755@gmail.com",
+//     subject:"Email Confirmation: nsk1755.com",
+//     text:"Random Value: " + randomValue
+//   }
+
+//   transporter.sendMail(mailOptions, function(error, res) {
+//     if(error) {
+//       console.log(error)
+//     }
+//     else {
+//       console.log('Send email!' + res.response)
+//     }
+    
+//     })
 // })
 
+
+app.get('/weather', (req, res) => {
+  // console.log('trynna get the weather')
+  // console.log(req.query.city + req.query.state + req.query.country) 
+  // https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + weatherKey
+  var promise = 'https://api.openweathermap.org/data/2.5/weather?q='+ req.query.city + ',' + req.query.state + ',' + req.query.country + '&units=imperial&APPID=' + process.env.WEATHER_KEY
+  // console.log(promise)
+  fetch(promise).then(
+    (response) => response.json()
+  ).then(
+    (jsonResponse) => {
+      // console.log(jsonResponse)
+      var weatherIcon = "https://openweathermap.org/img/w/" + jsonResponse.weather[0].icon + ".png";
+      var weather = jsonResponse.weather[0].description;
+      // console.log(weather)
+      var temperature = Math.ceil(jsonResponse.main.temp) + "\xB0 F";
+      // console.log(temperature)
+      var city = jsonResponse.name;
+      // console.log(city)
+      var country = jsonResponse.sys.country;
+      // console.log(country)
+
+      res.send({icon: weatherIcon, weather: weather, temp: temperature, city: city, country: country})
+    }
+  )
+})
+
 app.get('/ip', (req, res) => {
-    // res.set('Access-Control-Allow-Origin', 'http://localhost:'+4200);
-    // var ip = req.ip
+    // NOT GOOD FOR TESTING
     var ip = req.clientIp;
-    // var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+
+    //FOR TESTING USE AN ACTUAL IP
     var token = process.env.IP_INFO_KEY
     var promise = "https://ipinfo.io/" + ip + "/json?token=" + token
-    // console.log(promise)
 
     fetch(promise).then(
       (response) => response.json()
     ).then(
       (jsonResponse) => {
-        // console.log(jsonResponse.ip, jsonResponse.country)
         foundlocation = jsonResponse.city + ", " + jsonResponse.region + ", " + jsonResponse.country
-        // console.log('Location in fxn: ' + foundlocation)
+        const transporter = nodemailer.createTransport({
+          service:"gmail",
+          auth: {
+            user:"nodejsserverstuff@gmail.com",
+            pass:process.env.SERVER_KEY
+          }
+        })
+
+        const mailOptions = {
+          from:"nodejsserverstuff@gmail.com",
+          to:"nikhilkulkarni1755@gmail.com",
+          subject:"Ip Accessed from " + foundlocation,
+          text:"Ip Accessed from " + foundlocation
+        }
+
+        transporter.sendMail(mailOptions, function(error, res) {
+          if(error) {
+            console.log(error)
+          }
+          else {
+            console.log('Send email!' + res.response)
+          }
+          
+          })
         res.send(foundlocation)
-      }
-    )
-})
+
+    })  
+  })
 
 app.get('/f1', (req, res) => {
   var promise = 'http://ergast.com/api/f1/2023/15/results.json'
@@ -64,17 +146,9 @@ app.get('*', (req, res) => {
   res.send('<h1>Nikhil Kulkarni has created this server</h1>')
 })
 
-// app.post('*', function(req, res, next) {
-//   if(getIP === null) {
-//     console.log('did not find ip address')
-//   }
-//   var data = req.body.params
-//   console.log(req.data)
+// https.createServer(app).listen(port, () => {
+//   console.log('Node Server is running @ https://localhost:4000')
 // })
-
-// app.use('/angular', require(express.static('frontend')))
-
-// app.use(express.static('frontend'))
 
 app.listen(port, () => {
     console.log('Node server is running @ http://localhost:4000')
